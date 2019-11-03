cbdc2ca3d0372990447e8ed08a75af3f47e33552
Nayeem Kazi <psubangladesh@gmail.com>
2019-11-04 01:47:58 +0600

    fix

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,11 +5,38 @@
 <div class="container">
     <div class="row">
         <div class="col-8" >
+           
             <img src="/storage/{{$post->image}}" class="w-auto" style="height: 90vh;">   
         </div>   
         <div class="col-4">
-            <h1>{{$post->user->username}}</h1>
-            <p>{{$post->caption}}</p>
+            <div class="d-flex align-items-center">
+                <div>
+                    <img src="/storage/{{$post->user->profile->image}}" alt="besh to" class="rounded-circle"
+                    style="height: 50px;">
+                </div>
+                <div class="d-flex align-items-center">
+                    <p class="pl-3 bold h3"> 
+                        <a href="/profile/{{$post->user->id}}"> 
+                            <span class="text-dark">{{$post->user->username}}</span>
+                         </a> 
+                    </p>
+                    <a href="#" class="pl-3">Follow</a>
+                </div>
+                                
+            </div>
+            <hr>
+            <div>
+                
+                <p class="pt-3">
+                     <span > <strong> 
+                         <a href="/profile/{{$post->user->id}}"> 
+                            <span class="text-dark">{{$post->user->username}}</span> 
+                        </a> 
+                    </strong> </span> {{$post->caption}}
+                </p>
+
+            </div>
+            
         </div>
     </div>
 </div>
